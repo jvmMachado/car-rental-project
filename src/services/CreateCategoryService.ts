615a5ce020347115d3925b4684c6a00a7f230bcd
João Vitor Machado <joao.machado@somagrupo.com.br>
2022-02-26 13:06:52 -0300

    create ICategoriesRepository to serve as a contract to categories repositores implementations + add PostgresCategoriesRepository

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
 
 interface IRequest {
   name: string;
@@ -6,8 +6,9 @@ interface IRequest {
 }
 
 class CreateCategoryService {
-  private categoriesRepository: CategoriesRepository;
-  constructor(categoriesRepository: CategoriesRepository) {
+  private categoriesRepository: ICategoriesRepository;
+
+  constructor(categoriesRepository: ICategoriesRepository) {
     this.categoriesRepository = categoriesRepository;
   }
 
