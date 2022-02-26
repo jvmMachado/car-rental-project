70e077e23b107eb3bf3865e3ba80da5790068183
João Vitor Machado <joao.machado@somagrupo.com.br>
2022-02-25 23:14:47 -0300

    create CategoriesRepository and add create() method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,17 @@
 import { Router } from 'express';
 
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
 
-  const newCategory = new Category({
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ name, description });
 
-  categories.push(newCategory);
-
-  return response.status(201).json(newCategory);
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
