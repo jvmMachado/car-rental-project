7607bfb98b6d1b6f331fda097b8029452b52faef
João Vitor Machado <joao.machado@somagrupo.com.br>
2022-02-26 19:33:30 -0300

    create repositories implementations folder and adjust SpecificationsRepository to use singleton pattern

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import { SpecificationsRepository } from '../modules/cars/repositories/implementations/SpecificationsRepository';
 import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
+const specificationsRepository = SpecificationsRepository.getInstance();
 
 specificationsRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
